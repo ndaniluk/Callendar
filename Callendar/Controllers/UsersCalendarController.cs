93711563ea7c40802348fb775495ba71a536a7e8
Katarzyna Czerwinska <katarzyna.czerwinska@zeiss.com>
2019-12-20 10:42:51 +0100

    UsersCalendarController added

---
+++ Callendar/Controllers/UsersCalendarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Callendar.Controllers
+{
+	[Route("users")]
+	[ApiController]
+	public class UsersCalendarController : ControllerBase
+	{
+		private readonly CallendarDbContext _context;
+
+		public UsersCalendarController(CallendarDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: users/{guid}/calendar
+		[HttpGet("{guid}/calendar")]
+		public async Task<ActionResult<User>> GetUserCalendar(Guid guid)
+		{
+			//TODO
+			return null;
+		}
+	}
+}

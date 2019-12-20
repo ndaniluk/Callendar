529d458bf8ed092aa5ceefc8721da0cfc7b821b6
Katarzyna Czerwinska <katarzyna.czerwinska@zeiss.com>
2019-12-20 10:59:04 +0100

    UsersTasksController added

---
+++ Callendar/Controllers/UsersTasksController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Callendar.Controllers
+{
+	[Route("users")]
+	[ApiController]
+	public class UsersTasksController : ControllerBase
+	{
+		private readonly CallendarDbContext _context;
+
+		public UsersTasksController(CallendarDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: users/{guid}/tasks
+		[HttpGet("{guid}/tasks")]
+		public async Task<ActionResult<IEnumerable<Task>>> GetUserTasks(Guid guid)
+		{
+			//TODO
+			return null;
+		}
+
+		// PUT: users/{guid}/tasks/{taskId}
+		[HttpPut("{guid}/tasks/{taskId}")]
+		public async Task<ActionResult> PutTaskCompleting(Guid guid, int taskId)
+		{
+			//TODO
+			return null;
+		}
+
+		// POST: users/{guid}/tasks/{taskCategoryId}/telemarketer/{telemarketerGuid}
+		[HttpPost("{guid}/tasks/{taskCategoryId}/telemarketer/{telemarketerGuid}")]
+		public async Task<ActionResult> PostTasksForTelemarketer(Guid guid, int taskCategoryId, Guid telemarketerGuid)
+		{
+			//TODO
+			return null;
+		}
+	}
+}

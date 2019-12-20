8617eb565527ec1a421e583548a9d09f006013ab
Katarzyna Czerwinska <katarzyna.czerwinska@zeiss.com>
2019-12-20 09:53:21 +0100

    Login controller added

---
+++ Callendar/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Callendar.Controllers
+{
+	[Route("login")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+		private readonly CallendarDbContext _context;
+
+		public LoginController(CallendarDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public ActionResult<Guid> AuthorizeUser(User usersEmailAndPassword)
+		{
+			var users = _context.Users.Where(u =>
+				u.Email.Equals(usersEmailAndPassword.Email) && 
+				u.Password.Equals(usersEmailAndPassword.Password));
+
+			return users.Any() ? (ActionResult<Guid>)Ok(users.First().Id) : (ActionResult<Guid>)Unauthorized();
+		}
+	}
+}
--- Callendar/Controllers/UsersController.cs
+++ Callendar/Controllers/UsersController.cs
@@ -2,104 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Callendar;
 
 namespace Callendar.Controllers
 {
-    [Route("api/users")]
-    [ApiController]
-    public class UsersController : ControllerBase
-    {
-        private readonly CallendarDbContext _context;
+	[Route("users")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly CallendarDbContext _context;
 
-        public UsersController(CallendarDbContext context)
-        {
-            _context = context;
-        }
+		public UsersController(CallendarDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-        {
-            return await _context.Users.ToListAsync();
-        }
+		// GET: users
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+		{
+			return await _context.Users.ToListAsync();
+		}
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(Guid id)
-        {
-            var user = await _context.Users.FindAsync(id);
+		// GET: users/{guid}
+		[HttpGet("{guid}")]
+		public async Task<ActionResult<User>> GetUser(Guid guid)
+		{
+			var user = await _context.Users.FindAsync(guid);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+			if (user == null)
+			{
+				return NotFound();
+			}
 
-            return user;
-        }
+			return user;
+		}
 
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(Guid id, User user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
+		// PUT: users/{guguid}
+		[HttpPut("{guid}")]
+		public async Task<IActionResult> PutUser(Guid guid, User user)
+		{
+			if (guid != user.Id)
+			{
+				return BadRequest();
+			}
 
-            _context.Entry(user).State = EntityState.Modified;
+			_context.Entry(user).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!UserExists(guid))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-            return NoContent();
-        }
+			return NoContent();
+		}
 
-        // POST: api/Users
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+		// POST: users
+		[HttpPost]
+		public async Task<ActionResult<User>> PostUser(User user)
+		{
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
-        }
+			return CreatedAtAction("GetUser", new { id = user.Id }, user);
+		}
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<User>> DeleteUser(Guid id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+		// DELETE: users/{guid}
+		[HttpDelete("{guid}")]
+		public async Task<ActionResult<User>> DeleteUser(Guid guid)
+		{
+			var user = await _context.Users.FindAsync(guid);
+			if (user == null)
+			{
+				return NotFound();
+			}
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+			_context.Users.Remove(user);
+			await _context.SaveChangesAsync();
 
-            return user;
-        }
+			return user;
+		}
 
-        private bool UserExists(Guid id)
-        {
-            return _context.Users.Any(e => e.Id == id);
-        }
-    }
+		private bool UserExists(Guid guid)
+		{
+			return _context.Users.Any(e => e.Id == guid);
+		}
+	}
 }

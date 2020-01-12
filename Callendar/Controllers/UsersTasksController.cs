b62bf045ba307dcff1eddde1f1d636f87a76c6fb
Katarzyna Czerwińska <s17098@pjwstk.edu.pl>
2020-01-12 15:42:55 +0100

    UsersTasksController implementation

--- Callendar/Controllers/UsersTasksController.cs
+++ Callendar/Controllers/UsersTasksController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Callendar.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Callendar.Controllers
 {
@@ -16,28 +19,184 @@ namespace Callendar.Controllers
             _context = context;
         }
 
+        // GET: users/{guid}
+        [HttpGet("{guid}")]
+        public async Task<ActionResult<User>> GetUserInfo(Guid guid)
+        {
+            var user = await _context.Users
+                .Where(x => x.Id == guid).SingleOrDefaultAsync();
+
+            if (user == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(user);
+        }
+
+        // GET: users/{guid}/telemarketerStatistics
+        [HttpGet("{guid}/telemarketerStatistics")]
+        public async Task<ActionResult<TelemarketerStatistics>> GetTelemarketerStatistics(Guid guid)
+        {
+            var user = await _context.Users
+                .Where(x => x.Id == guid).SingleOrDefaultAsync();
+
+            if (user == null)
+            {
+                return new NotFoundResult();
+            }
+
+
+            var tasks = await _context.Tasks
+                .Where(x => x.UserId == guid)
+                .Include(x => x.TaskCategory)
+                .ToListAsync();
+
+            int pointsToGet = 0;
+
+            foreach (var task in tasks.Where(t => t.IsClosed == false))
+                pointsToGet += task.TaskCategory.ScorePoints;
+
+            TelemarketerStatistics telemarketerStatistics = new TelemarketerStatistics();
+
+            telemarketerStatistics.PointsToGet = pointsToGet;
+            telemarketerStatistics.TasksToDoCount = tasks.Where(x => x.IsClosed == false).Count();
+            telemarketerStatistics.TasksDoneCount = tasks.Where(x => x.IsClosed).Count();
+            telemarketerStatistics.Points = user.Points;
+
+            return new OkObjectResult(telemarketerStatistics);
+        }
+
+        // GET: users/{guid}/teamMembers
+        [HttpGet("{guid}/teamMembers")]
+        public async Task<ActionResult<IEnumerable<User>>> GetTeamMembers(Guid guid)
+        {
+            var leader = await _context.Users.Where(x => x.Id == guid).SingleOrDefaultAsync();
+
+            if (leader == null)
+            {
+                return new NotFoundResult();
+            }
+
+            var teamMembers = await _context.Users
+                .Where(x => x.TeamId == leader.TeamId && x.PositionId == 1)
+                .ToListAsync();
+
+            if (teamMembers == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(teamMembers);
+        }
+
+        // GET: users/{guid}/tasksCategories
+        [HttpGet("{guid}/tasksCategories")]
+        public async Task<ActionResult<IEnumerable<Task>>> GetTasksCategories(Guid guid)
+        {
+            var tasksCategories = await _context.TaskCategories
+                 .ToListAsync();
+
+            if (tasksCategories == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(tasksCategories);
+        }
+
         // GET: users/{guid}/tasks
         [HttpGet("{guid}/tasks")]
-        public async Task<ActionResult<IEnumerable<Task>>> GetUserTasks(Guid guid)
+        public async Task<ActionResult<IEnumerable<Task>>> GetTelemarketerTasks(Guid guid)
         {
-            //TODO
-            return null;
+            var tasks = await _context.Tasks
+                .Where(x => x.UserId == guid)
+                .ToListAsync();
+
+            if (tasks == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(tasks);
         }
 
         // PUT: users/{guid}/tasks/{taskId}
         [HttpPut("{guid}/tasks/{taskId}")]
         public async Task<ActionResult> PutTaskCompleting(Guid guid, int taskId)
         {
-            //TODO
-            return null;
+            var user = await _context.Users
+                .Where(x => x.Id == guid)
+                .SingleOrDefaultAsync();
+
+            if (user == null)
+            {
+                return new NotFoundResult();
+            }
+
+
+            var task = await _context.Tasks
+                .Where(x => x.Id == taskId)
+                .Include(x => x.TaskCategory)
+                .SingleOrDefaultAsync();
+
+            if (task == null)
+            {
+                return new NotFoundResult();
+            }
+
+            task.IsClosed = true;
+            user.Points += task.TaskCategory.ScorePoints;
+
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return Ok();
         }
 
         // POST: users/{guid}/tasks/{taskCategoryId}/telemarketer/{telemarketerGuid}
         [HttpPost("{guid}/tasks/{taskCategoryId}/telemarketer/{telemarketerGuid}")]
         public async Task<ActionResult> PostTasksForTelemarketer(Guid guid, int taskCategoryId, Guid telemarketerGuid)
         {
-            //TODO
-            return null;
+            var user = await _context.Users
+                .Where(x => x.Id == telemarketerGuid)
+                .SingleOrDefaultAsync();
+
+            if (user == null)
+            {
+                return new NotFoundResult();
+            }
+
+
+            var taskCategory = await _context.Tasks
+                .Where(x => x.Id == taskCategoryId)
+                .SingleOrDefaultAsync();
+
+            if (taskCategory == null)
+            {
+                return new NotFoundResult();
+            }
+
+            var task = new Task()
+            {
+                IsClosed = false,
+                TaskCategoryId = taskCategoryId,
+                UserId = telemarketerGuid
+            };
+
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
---
+++ Callendar/Models/TelemarketerStatistics.cs
@@ -0,0 +1,10 @@
+﻿namespace Callendar.Models
+{
+    public class TelemarketerStatistics
+    {
+        public int TasksToDoCount { get; set; }
+        public int TasksDoneCount { get; set; }
+        public int PointsToGet { get; set; }
+        public int Points { get; set; }
+    }
+}
